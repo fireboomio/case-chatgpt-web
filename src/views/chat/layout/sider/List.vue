5300bb80dfcbcc0874bdad4c312867043fa64954
erguotou <erguotou525@gmail.com>
2023-03-28 10:55:14 +0800

    fix: build fix

--- src/views/chat/layout/sider/List.vue
+++ src/views/chat/layout/sider/List.vue
@@ -1,11 +1,10 @@
 <script setup lang='ts'>
-import {computed, watch} from 'vue'
-import {NInput, NPopconfirm, NScrollbar, useMessage} from 'naive-ui'
+import { computed } from 'vue'
+import { NInput, NPopconfirm, NScrollbar, useMessage } from 'naive-ui'
+import { useRoute } from 'vue-router'
 import { SvgIcon } from '@/components/common'
 import { useAppStore, useChatStore } from '@/store'
 import { useBasicLayout } from '@/hooks/useBasicLayout'
-import {t} from "@/locales";
-import {useRoute, useRouter} from "vue-router";
 
 const { isMobile } = useBasicLayout()
 
@@ -14,7 +13,7 @@ const message = useMessage()
 const appStore = useAppStore()
 const chatStore = useChatStore()
 const route = useRoute()
-const uuid = computed(() => route.params.uuid)
+// const uuid = computed(() => route.params.uuid)
 
 const dataSources = computed(() => chatStore.history)
 chatStore.setActive(Number(route.params.uuid))
@@ -32,9 +31,9 @@ async function handleSelect({ uuid }: Chat.History) {
 
 function handleEdit({ uuid, title }: Chat.History, isEdit: boolean, event?: MouseEvent) {
   event?.stopPropagation()
-  if (!title) {
+  if (!title)
     return message.error('名称不能为空')
-  }
+
   chatStore.updateHistory(uuid, { isEdit })
 }
 
@@ -56,9 +55,9 @@ function isActive(uuid: number) {
 
 <template>
   <NScrollbar class="px-4">
-    <div class="flex flex-col gap-2 text-sm">
+    <div class="flex flex-col text-sm gap-2">
       <template v-if="!dataSources.length">
-        <div class="flex flex-col items-center mt-4 text-center text-neutral-300">
+        <div class="flex flex-col mt-4 text-center text-neutral-300 items-center">
           <SvgIcon icon="ri:inbox-line" class="mb-2 text-3xl" />
           <span>{{ $t('common.noData') }}</span>
         </div>
@@ -66,14 +65,14 @@ function isActive(uuid: number) {
       <template v-else>
         <div v-for="(item, index) of dataSources" :key="index">
           <a
-            class="relative flex items-center gap-3 px-3 py-3 break-all border rounded-md cursor-pointer hover:bg-neutral-100 group dark:border-neutral-800 dark:hover:bg-[#24272e]"
+            class="border rounded-md cursor-pointer flex py-3 px-3 gap-3 relative items-center break-all group dark:border-neutral-800 hover:bg-neutral-100 dark:hover:bg-[#24272e]"
             :class="isActive(item.uuid) && ['border-[#4b9e5f]', 'bg-neutral-100', 'text-[#4b9e5f]', 'dark:bg-[#24272e]', 'dark:border-[#4b9e5f]', 'pr-14']"
             @click="handleSelect(item)"
           >
             <span>
               <SvgIcon icon="ri:message-3-line" />
             </span>
-            <div class="relative flex-1 overflow-hidden break-all text-ellipsis whitespace-nowrap">
+            <div class="flex-1 text-ellipsis relative overflow-hidden break-all whitespace-nowrap">
               <NInput
                 v-if="item.isEdit"
                 v-model:value="item.title"
@@ -82,7 +81,7 @@ function isActive(uuid: number) {
               />
               <span v-else>{{ item.title }}</span>
             </div>
-            <div v-if="isActive(item.uuid)" class="absolute z-10 flex visible right-1">
+            <div v-if="isActive(item.uuid)" class="flex right-1 z-10 absolute visible">
               <template v-if="item.isEdit">
                 <button class="p-1" @click="handleEdit(item, false, $event)">
                   <SvgIcon icon="ri:save-line" />
